7f05ed68185c25ea60813ae786ffc426e5d4b44f
Monica Craveiro <craveiromonica@gmail.com>
2024-10-02 09:25:32 -0300

    Criand seed

--- db/migrate/20240927053631_rename_url_image_to_url_image_in_coins.rb
+++ db/migrate/20240927053631_rename_url_image_to_url_image_in_coins.rb
@@ -1,4 +1,4 @@
-class RenameUrlimageToUrlImageInCoins < ActiveRecord::Migration[7.2]
+class RenameUrlImageToUrlImageInCoins < ActiveRecord::Migration[7.2]
   def change
     rename_column :coins, :url_image, :url_image
   end
