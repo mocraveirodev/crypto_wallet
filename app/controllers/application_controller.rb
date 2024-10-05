42eb7684dea0d4c065a051eff84db2360f3e38d7
Monica Craveiro <craveiromonica@gmail.com>
2024-10-05 09:16:31 -0300

    Cookie para localização.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end
 end
